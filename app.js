acfa9413bbb96a7434dce486e05fcdfc5f664696
joori-fe <68975884+joori-fe@users.noreply.github.com>
2020-08-29 15:37:20 +0900

    #2.3 Changing Color (06:19)

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext('2d');
+const colors = document.getElementsByClassName("jsColor");
 
 canvas.width=700;
 canvas.height=500;
@@ -29,19 +30,26 @@ function onMouseMove(event){
     }
 }
 
-function onMouseDown(event){
-    painting = true;
-}
-
+// function onMouseDown(event){
+//     painting = true;
+// }
 // function onMouseUp(event){
 //     stopPainting();
 // }
 // function onMouseLeave(event){
 //     stopPainting();
 // }
+
+function handleColorClick(event){
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
+    // console.log(event.target.style.backgroundColor)
+}
 if(canvas){
     canvas.addEventListener("mousemove",onMouseMove)
     canvas.addEventListener("mousedown",startPainting)
     canvas.addEventListener("mouseup",stopPainting)
     canvas.addEventListener("mouseleave",stopPainting)
 }
+Array.from(colors).forEach(color=>color.addEventListener("click",handleColorClick))
+console.log(Array.from(colors));
